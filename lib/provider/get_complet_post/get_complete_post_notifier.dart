e5048fbad8a080d138797c98b4d6f094e2d121a3
waiphyoaung <rubyland.myanmar.mg@gmail.com>
2023-06-27 17:57:44 +0630

    get complete post notifier class add

---
+++ lib/provider/get_complet_post/get_complete_post_notifier.dart
@@ -0,0 +1,22 @@
+import 'package:blog_rest_api_provider/data/model/get_one_post_response.dart';
+import 'package:blog_rest_api_provider/data/service/blog_api_service.dart';
+import 'package:blog_rest_api_provider/provider/get_complet_post/get_complete_post_state.dart';
+import 'package:flutter/widgets.dart';
+
+class GetCompletePostNotifier extends ChangeNotifier{
+  GetCompletePostState getCompletePostState = GetCompletePostLoading();
+  final BlogApiService _blogApiService = BlogApiService();
+  void getCompletePost({required int id}) async{
+    getCompletePostState = GetCompletePostLoading();
+    notifyListeners();
+    try {
+      GetOnePostResponse getOnePostResponse = await _blogApiService.getOnePost(
+          id);
+      getCompletePostState = GetCompletePostSuccess(getOnePostResponse);
+      notifyListeners();
+    }catch(_){
+      getCompletePostState = GetCompletePostFailed('Something wrong');
+    }
+
+  }
+}
---
+++ lib/provider/get_complet_post/get_complete_post_state.dart
@@ -0,0 +1,13 @@
+import 'package:blog_rest_api_provider/data/model/get_one_post_response.dart';
+
+abstract class GetCompletePostState{}
+class GetCompletePostLoading extends GetCompletePostState{}
+
+class GetCompletePostSuccess extends GetCompletePostState{
+  final GetOnePostResponse getOnePostResponse;
+  GetCompletePostSuccess(this.getOnePostResponse);
+}
+class GetCompletePostFailed extends GetCompletePostState{
+  final String errorMessage;
+  GetCompletePostFailed(this.errorMessage);
+}
